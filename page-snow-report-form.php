de06b3086320ffbfa1bae751931e5f0bd6894775
mahdi-mim <mahdiroozbahani25@gmail.com>
2025-07-03 17:07:59 -0700

    Add front-end form template for updating trail statuses (Snow Report Form)

--- page-snow-report-form.php
+++ page-snow-report-form.php
@@ -16,23 +16,44 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
+		<h1>Update Trail Status</h1>
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+		<form method="post">
+			<?php
+			$trails = get_posts([
+				'post_type' => 'trail',
+				'numberposts' => '-1',
+				'orderby' => 'title',
+				'order' => 'ASC'
+			]);
 
-			get_template_part( 'template-parts/content', 'page' );
+			foreach ($trails as $trail) {
+				$status = get_field('status', $trail->ID);
+				?>
+				<label for="trail_<?php echo $trail->ID; ?>">
+					<?php echo esc_html($trail->post_title); ?>
+				</label>
+				  <select name="trail_status[<?php echo $trail->ID; ?>]" id="trail_<?php echo $trail->ID; ?>">
+                    <option value="open" <?php selected($status, 'open'); ?>>Open</option>
+                    <option value="closed" <?php selected($status, 'closed'); ?>>Closed</option>
+                  </select>
+				  <br><br>
+                <?php
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			}
+			 ?>
+		</form>
 
-		endwhile; // End of the loop.
-		?>
+	  <?php
+       if (isset($_POST['submit_status']) && isset($_POST['trail_status'])) {
+       foreach ($_POST['trail_status'] as $trail_id => $new_status) {
+       update_field('status', $new_status, intval($trail_id));
+    }
+    echo "<p style='color: green;'>Status updated!</p>";
+  }
+  ?>
 
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
