169306ae2e4a9c8fabc17154fcadab2ef23ac0e5
mahdi-mim <mahdiroozbahani25@gmail.com>
2025-07-04 16:55:23 -0700

    added svg code for trail map

---
+++ template-parts/trail-map.php
@@ -0,0 +1,38 @@
+<svg viewBox="0 0 1200 1000" xmlns="http://www.w3.org/2000/svg" class="trail-map">
+
+  <polygon 
+    class="trail-shape" 
+    id="orchard" 
+    data-trail="Orchard"
+    points="402,342 368,682 383,683 416,344"
+  />
+
+  <polygon 
+    class="trail-shape" 
+    id="south" 
+    data-trail="South"
+    points="547,642 550,377 561,375 560,641"
+  />
+
+  <polygon 
+    class="trail-shape" 
+    id="village" 
+    data-trail="Village"
+    points="754,425 743,425 728,655 743,658"
+  />
+
+  <polygon 
+    class="trail-shape" 
+    id="valley" 
+    data-trail="Valley"
+    points="938,474 925,469 899,672 910,673"
+  />
+
+  <polygon 
+    class="trail-shape" 
+    id="north" 
+    data-trail="North"
+    points="1086,472 1096,468 1112,668 1098,668"
+  />
+
+</svg>
