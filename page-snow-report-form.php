d1ddb83a137a5d8cc3d2ddf80269343e03ef1a45
mahdi-mim <mahdiroozbahani25@gmail.com>
2025-07-03 17:08:59 -0700

    Add front-end form template for updating trail statuses (Snow Report Form)

--- page-snow-report-form.php
+++ page-snow-report-form.php
@@ -12,48 +12,52 @@
  * @package SnowTrail
  */
 
+if (isset($_POST['submit_status']) && isset($_POST['trail_status'])) {
+  foreach ($_POST['trail_status'] as $trail_id => $new_status) {
+    update_field('status', sanitize_text_field($new_status), intval($trail_id));
+  }
+
+  wp_redirect(add_query_arg('updated', 'true', get_permalink()));
+  exit;
+}
+
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-		<h1>Update Trail Status</h1>
+<main id="primary" class="site-main">
+  <h1>Update Trail Status</h1>
 
-		<form method="post">
-			<?php
-			$trails = get_posts([
-				'post_type' => 'trail',
-				'numberposts' => '-1',
-				'orderby' => 'title',
-				'order' => 'ASC'
-			]);
+  <?php if (isset($_GET['updated']) && $_GET['updated'] === 'true'): ?>
+    <p style="color: green;">Status updated!</p>
+  <?php endif; ?>
 
-			foreach ($trails as $trail) {
-				$status = get_field('status', $trail->ID);
-				?>
-				<label for="trail_<?php echo $trail->ID; ?>">
-					<?php echo esc_html($trail->post_title); ?>
-				</label>
-				  <select name="trail_status[<?php echo $trail->ID; ?>]" id="trail_<?php echo $trail->ID; ?>">
-                    <option value="open" <?php selected($status, 'open'); ?>>Open</option>
-                    <option value="closed" <?php selected($status, 'closed'); ?>>Closed</option>
-                  </select>
-				  <br><br>
-                <?php
+  <form method="post">
+    <?php
+    $trails = get_posts([
+      'post_type' => 'trail',
+      'numberposts' => -1,
+      'orderby' => 'title',
+      'order' => 'ASC'
+    ]);
 
-			}
-			 ?>
-		</form>
+    foreach ($trails as $trail) {
 
-	  <?php
-       if (isset($_POST['submit_status']) && isset($_POST['trail_status'])) {
-       foreach ($_POST['trail_status'] as $trail_id => $new_status) {
-       update_field('status', $new_status, intval($trail_id));
+      $status = get_field('status', $trail->ID);
+      ?>
+      <label for="trail_<?php echo $trail->ID; ?>">
+        <?php echo esc_html($trail->post_title); ?>
+      </label>
+      <select name="trail_status[<?php echo $trail->ID; ?>]" id="trail_<?php echo $trail->ID; ?>">
+        <option value="open" <?php selected($status, 'open'); ?>>Open</option>
+        <option value="closed" <?php selected($status, 'closed'); ?>>Closed</option>
+      </select>
+      <br><br>
+      <?php
     }
-    echo "<p style='color: green;'>Status updated!</p>";
-  }
-  ?>
-
-	</main><!-- #main -->
+    ?>
+    <input type="submit" name="submit_status" value="Save Status">
+  </form>
+</main><!-- #main -->
 
 <?php
 get_footer();
